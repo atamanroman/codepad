b4528dc353327cb69bfc767ccbeac65e0b3ae4a1
Roman Ataman <atamanroman@work>
2013-10-07 00:40:36 +0200

    Save and show snippets

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,10 +1,25 @@
 class SnippetsController < ApplicationController
 
+  def index
+    redirect_to action: :new
+  end
+
   def new
   end
 
   def create
-    render text: params[:snippet].inspect
+    @snippet = Snippet.new(snippet_params)
+
+    redirect_to @snippet if @snippet.save
   end
+
+  def show
+    @snippet = Snippet.find(params[:id])
+  end
+
+  private
+    def snippet_params
+      params.require(:snippet).permit(:author, :code)
+    end
 
 end
