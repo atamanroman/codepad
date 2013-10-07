add5dd370e2a3ad7479fd49a8f96fb5a9a5a46dc
Roman Ataman <atamanroman@work>
2013-10-07 23:55:34 +0200

    Edit form and partial forms

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -5,21 +5,41 @@ class SnippetsController < ApplicationController
   end
 
   def new
+    @snippet = Snippet.new
   end
 
   def create
-    @snippet = Snippet.new(snippet_params)
-
-    redirect_to @snippet if @snippet.save
+    from_params
+    @snippet.save
+    redirect_to @snippet
   end
 
   def show
-    @snippet = Snippet.find(params[:id])
+    from_params_id
+  end
+
+  def edit
+    from_params_id
+  end
+
+  def update
+    from_params_id
+    @snippet.update(snippet_params)
+    redirect_to @snippet
   end
 
   private
+    def from_params
+      @snippet = Snippet.new(snippet_params)
+    end
+
+    def from_params_id
+      @snippet = Snippet.find(params[:id])
+    end
+
     def snippet_params
       params.require(:snippet).permit(:author, :code)
     end
+
 
 end
